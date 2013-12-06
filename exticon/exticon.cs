5fbb91de55095a0f0ae5b397f7ff638e434de9db
mattn <mattn.jp@gmail.com>
2013-12-06 14:20:51 +0900

    Add tool

---
+++ exticon/exticon.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+class ExtractIcon
+{
+	private const uint SHGFI_ICON = 0x100;
+	private const uint SHGFI_LARGEICON = 0x0;
+	private const uint SHGFI_SMALLICON = 0x1;
+    private const uint SHGFI_SYSICONINDEX = 0x4000;
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SHFILEINFO
+	{
+		public IntPtr hIcon;
+		public IntPtr iIcon;
+		public uint dwAttributes;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+		public string szDisplayName;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+		public string szTypeName;
+	};
+
+    [ComImport, Guid("0000010c-0000-0000-c000-000000000046"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IPersist
+    {
+        [PreserveSig]
+        void GetClassID(out Guid pClassID);
+    }
+
+    [ComImport, Guid("0000010b-0000-0000-C000-000000000046"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IPersistFile : IPersist
+    {
+        new void GetClassID(out Guid pClassID);
+        [PreserveSig]
+        int IsDirty();
+
+        [PreserveSig]
+        void Load([In, MarshalAs(UnmanagedType.LPWStr)]
+        string pszFileName, uint dwMode);
+
+        [PreserveSig]
+        void Save([In, MarshalAs(UnmanagedType.LPWStr)] string pszFileName,
+            [In, MarshalAs(UnmanagedType.Bool)] bool fRemember);
+
+        [PreserveSig]
+        void SaveCompleted([In, MarshalAs(UnmanagedType.LPWStr)] string pszFileName);
+
+        [PreserveSig]
+        void GetCurFile([In, MarshalAs(UnmanagedType.LPWStr)] string ppszFileName);
+    }
+
+	[DllImport("shell32.dll")]
+	public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+    [
+        ComImport(),
+        Guid("00021401-0000-0000-C000-000000000046")
+    ]
+    public class ShellLink
+    {
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    struct WIN32_FIND_DATAW
+    {
+        public uint dwFileAttributes;
+        public long ftCreationTime;
+        public long ftLastAccessTime;
+        public long ftLastWriteTime;
+        public uint nFileSizeHigh;
+        public uint nFileSizeLow;
+        public uint dwReserved0;
+        public uint dwReserved1;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string cFileName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
+        public string cAlternateFileName;
+    }
+
+    [Flags()]
+    enum SLR_FLAGS
+    {
+        SLR_NO_UI = 0x1,
+        SLR_ANY_MATCH = 0x2,
+        SLR_UPDATE = 0x4,
+        SLR_NOUPDATE = 0x8,
+        SLR_NOSEARCH = 0x10,
+        SLR_NOTRACK = 0x20,
+        SLR_NOLINKINFO = 0x40,
+        SLR_INVOKE_MSI = 0x80
+    }
+
+    [Flags()]
+    enum SLGP_FLAGS
+    {
+        SLGP_SHORTPATH = 0x1,
+        SLGP_UNCPRIORITY = 0x2,
+        SLGP_RAWPATH = 0x4
+    }
+
+    [ComImport(), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("000214F9-0000-0000-C000-000000000046")]
+    interface IShellLinkW
+    {
+        void GetPath([Out(), MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out WIN32_FIND_DATAW pfd, SLGP_FLAGS fFlags);
+        void GetIDList(out IntPtr ppidl);
+        void SetIDList(IntPtr pidl);
+        void GetDescription([Out(), MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+        void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+        void GetWorkingDirectory([Out(), MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+        void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+        void GetArguments([Out(), MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+        void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+        void GetHotkey(out short pwHotkey);
+        void SetHotkey(short wHotkey);
+        void GetShowCmd(out int piShowCmd);
+        void SetShowCmd(int iShowCmd);
+        void GetIconLocation([Out(), MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+        void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+        void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, int dwReserved);
+        void Resolve(IntPtr hwnd, SLR_FLAGS fFlags);
+        void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+    }
+
+    private const uint STGM_READ = 0;
+    private const int MAX_PATH = 260;
+    public static string ResolveShortcut(string filename)
+    {
+        ShellLink link = new ShellLink();
+        ((IPersistFile)link).Load(filename, STGM_READ);
+        StringBuilder sb = new StringBuilder(MAX_PATH);
+        WIN32_FIND_DATAW data = new WIN32_FIND_DATAW();
+        ((IShellLinkW)link).GetPath(sb, sb.Capacity, out data, 0);
+        return sb.ToString();
+    }
+
+    static void Main(string[] args)
+	{
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Usage: exticon [file] [icon-file]");
+            return;
+        }
+        string filename = args[0];
+        if (filename.ToLower().EndsWith(".lnk"))
+        {
+            filename = ResolveShortcut(filename);
+        }
+		SHFILEINFO shinfo = new SHFILEINFO();
+    	SHGetFileInfo(filename, 0, ref shinfo, (uint) Marshal.SizeOf(shinfo), SHGFI_ICON | SHGFI_SMALLICON);
+		Icon icon = Icon.FromHandle(shinfo.hIcon);
+        using (FileStream fs = new FileStream(args[1], FileMode.Create))
+        {
+            icon.Save(fs);
+        }
+    }
+}
